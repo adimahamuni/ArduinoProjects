1bb833d55a219db7a3022e73fe2372dd5d8e6bc0
Aditya Mahamuni <43707436+adimahamuni@users.noreply.github.com>
2018-09-30 00:29:09 +0530

    Add files via upload

---
+++ Bluetooth/bt_commtn.ino
@@ -0,0 +1,28 @@
+char data;
+int button  =13;
+int butn;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  pinMode(button, INPUT);
+  digitalWrite(button,LOW);
+}
+
+void loop() {
+  if (Serial.available() > 0) // Send data only when you receive data:
+  {
+    data = Serial.read();
+  }
+  butn = digitalRead(button);
+    // put your main code here, to run repeatedly:
+    if (butn == HIGH)
+    {
+      Serial.write('1');
+      data = 1;
+    }
+    else if (butn == LOW)
+    {
+      Serial.write('0');
+      data = 0;
+    }
+  }
